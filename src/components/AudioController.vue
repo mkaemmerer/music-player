a9f39a37a265dc4ab106a10715ab59e89d82ac77
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2019-01-28 20:46:31 +0900

    feat: add audio element with controls

---
+++ src/components/AudioController.vue
@@ -0,0 +1,14 @@
+<template>
+  <audio
+      controls
+      src="../assets/t-rex-roar.mp3">
+          Your browser does not support the
+          <code>audio</code> element.
+  </audio>
+</template>
+
+<script>
+  export default {
+    name: 'AudioController'
+  }
+</script>
