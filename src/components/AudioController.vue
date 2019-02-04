d32bd658822d55374e70f49dbd85323a7758ab49
Tsubasa Kondo <tsubasa.k111@gmail.com>
2019-02-04 21:58:03 +0900

    feat: add play/pause control

--- src/components/AudioController.vue
+++ src/components/AudioController.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <audio controls :src="$store.state.audioFile">
+    <audio ref="audio" controls :src="$store.state.audioFile">
       Your browser does not support the
       <code>audio</code> element.
     </audio>
+    <button @click="togglePlayback">
+      {{ $store.state.isPaused ? "play" : "pause" }}
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "AudioController"
+  name: "AudioController",
+  methods: {
+    togglePlayback() {
+      this.$store.state.isPaused
+        ? this.$refs.audio.play()
+        : this.$refs.audio.pause();
+      this.$store.dispatch("togglePlayback");
+    }
+  }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -5,16 +5,23 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    audioFile: null
+    audioFile: null,
+    isPaused: false
   },
   mutations: {
     setFile(state, newFile) {
       state.audioFile = newFile;
+    },
+    togglePlayback(state) {
+      state.isPaused = !state.isPaused;
     }
   },
   actions: {
     setFile(context, newFile) {
       context.commit("setFile", newFile);
+    },
+    togglePlayback(context) {
+      context.commit("togglePlayback");
     }
   }
 });
