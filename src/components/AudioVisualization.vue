3c99900fdf08cbcd75ab21e2819fdc0c79833d5a
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2019-03-12 21:21:51 +0900

    wip: visualization

--- src/components/AudioVisualization.vue
+++ src/components/AudioVisualization.vue
@@ -5,6 +5,12 @@
 </template>
 
 <script>
+import AudioSource from "../utils/audio-source";
+import VerticalFrequencyVis from "../visualizations/frequency-viz";
+
+const audioSource = new AudioSource();
+new VerticalFrequencyVis(audioSource.analyser);
+
 export default {
   name: "AudioVisualization",
 };
