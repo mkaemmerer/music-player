35b1ca7d2aba6d378ddbd6756406ce0899e95be8
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2019-03-12 20:07:40 +0900

    style: set up application layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <AudioController />
-    <AudioLoader />
+    <div class="sidebar">
+      <div class="sidebar-header">
+        <AudioLoader />
+      </div>
+      <div class="sidebar-main">
+      </div>
+      <div class="sidebar-footer">
+        <AudioController />
+      </div>
+    </div>
+    <div class="main">
+      <AudioVisualization />
+    </div>
   </div>
 </template>
 
 <script>
 import AudioController from "./components/AudioController.vue";
 import AudioLoader from "./components/AudioLoader.vue";
+import AudioVisualization from "./components/AudioVisualization.vue";
 
 export default {
   name: "app",
   components: {
     AudioController,
-    AudioLoader
+    AudioLoader,
+    AudioVisualization
   }
 };
 </script>
 
 <style>
+html, body {
+  margin: 0;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  height: 100vh;
+}
+
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+}
+.sidebar-header {
+}
+.sidebar-main {
+  flex: 1;
+}
+.sidebar-footer {
+}
+
+.main {
+  flex: 1;
 }
 </style>
---
+++ src/components/AudioVisualization.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="audio-visualization">
+    AudioVisualization
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AudioVisualization",
+};
+</script>
+
+<style>
+.audio-visualization {
+  background-color: #dddddd;
+  height: 100%;
+}
+</style>
